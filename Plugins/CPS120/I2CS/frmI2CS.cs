fffcefab1bc97c078e280bc05c20f2d61594d6ed
xushirui <xu@controlanything.com>
2015-12-01 11:07:47 -0600

    add more sensors

---
+++ Plugins/CPS120/I2CS/frmI2CS.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using AnyI2C;
+
+namespace CPS120_I2CS
+{
+    public partial class frmI2CS : Form
+    {
+        CommInterface CommObj = null;
+        public frmI2CS()
+        {
+            InitializeComponent();
+        }
+
+        private void frmADC081C_Load(object sender, EventArgs e)
+        {
+            numAddress.Value = CommObj.GetDefaultAddress();
+        }
+
+        public void Attach(CommInterface com)
+        {
+            CommObj = com;
+        }
+
+        /// <summary>
+        /// Send command, will process address
+        /// </summary>
+        private byte GetAddress(bool Read)
+        {
+            if (Read)
+            {
+                return (byte)(numAddress.Value * 2 + 1);
+            }
+            return (byte)(numAddress.Value * 2);
+        }
+
+        private string ReadSensor()
+        {
+            try
+            {
+                _ERROR.Visible = false;
+                bool readSuccess = false;
+                int tryTimes = 0;
+                while (!readSuccess && tryTimes < 10)
+                {
+                    tryTimes++;
+                    byte addr = GetAddress(false);
+                    byte[] value = CommObj.Send(new byte[] { addr }, 0);
+                    addr = GetAddress(true);
+                    value = CommObj.Send(new byte[] { addr }, 4);
+                    if (value != null)
+                    {
+                        if (value.Length == 4)
+                        {
+                            int v = value[0] & 0xC0;
+                            if (v <2)
+                            {
+                                readSuccess = true;
+                                double baro = (value[0] & 0x3f * 256 + value[1])*0.055 + 30 ;
+                                double temp = (value[2] * 64) * 0.01 - 40;
+                                lbBaro.Text = baro.ToString("F2");
+                                lbTemp.Text = temp.ToString("F2");
+                                return value[0].ToString();
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch
+            {
+                _ERROR.Visible = true;
+            }
+
+            return "Error";
+        }
+
+        private void btnReadCh0_Click(object sender, EventArgs e)
+        {
+            ReadSensor();
+        }
+
+
+    }
+
+
+    public class MyGUI : GuiInterface
+    {
+        public void Show(CommInterface com)
+        {
+            frmI2CS frm = new frmI2CS();
+            frm.Attach(com);
+            frm.ShowDialog();
+        }
+    }
+
+}
---
+++ Plugins/M24512/I2CMEM/frmI2CMEM.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AnyI2C;
+
+namespace M24512_I2CM
+{
+    public partial class frmI2CMem : Form
+    {
+        CommInterface CommObj = null;
+        int value1 = 0;
+        int value2 = 0;
+        int value3 = 0;
+        int value4 = 4;
+        public frmI2CMem()
+        {
+            InitializeComponent();
+        }
+        
+        public void Attach(CommInterface com)
+        {
+            CommObj = com;
+        }        
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            byte addr = GetAddress(false);
+            byte[] v = CommObj.Send(new byte [] {addr, (byte)(numAddrHigh.Value), (byte)(numAddrLow.Value)}, 1);
+            if (v != null)
+            {
+                numValue.Value = v[0];
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            byte addr = GetAddress(false);
+            CommObj.Send(new byte [] {addr, (byte)(numAddrHigh.Value), (byte)(numAddrLow.Value), (byte)(numValue.Value)}, 0);
+
+        }
+
+        /// <summary>
+        /// Send command, will process address
+        /// </summary>
+        private byte GetAddress(bool Read)
+        {
+            if (Read)
+            {
+                return (byte)(numAddress.Value * 2 + 1);
+            }
+            return (byte)(numAddress.Value * 2);
+        }
+
+        private void frmI2CMem_Load(object sender, EventArgs e)
+        {
+            numAddress.Value = CommObj.GetDefaultAddress();
+        }
+
+
+
+    }
+
+
+    public class MyGUI : GuiInterface
+    {
+        public void Show(CommInterface com)
+        {
+            frmI2CMem frm = new frmI2CMem();
+            frm.Attach(com);
+            frm.ShowDialog();
+        }
+    }
+}
