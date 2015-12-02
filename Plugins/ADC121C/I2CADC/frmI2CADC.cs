39bc25246d9ec1431da31714458f1f8b1d662783
xsr <xushirui@serailporttool.com>
2015-12-02 16:56:11 -0600

    add ad121c & ms5837

---
+++ Plugins/ADC121C/I2CADC/frmI2CADC.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AnyI2C;
+
+namespace ADC121C_I2CADC
+{
+    public partial class frmI2CADC : Form
+    {
+        CommInterface CommObj = null;
+        public frmI2CADC()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAD7999_Load(object sender, EventArgs e)
+        {
+            numAddress.Value = CommObj.GetDefaultAddress();
+        }
+
+        public void Attach(CommInterface com)
+        {
+            CommObj = com;
+        }
+
+        /// <summary>
+        /// Send command, will process address
+        /// </summary>
+        private byte GetAddress(bool Read)
+        {
+            if (Read)
+            {
+                return (byte)(numAddress.Value * 2 + 1);
+            }
+            return (byte)(numAddress.Value * 2);
+        }
+
+        private string ReadCh()
+        {
+            try
+            {
+                _ERROR.Visible = false;
+                byte addr = GetAddress(false);
+                byte[] value = CommObj.Send(new byte[] { addr, 0}, 2);
+                if (value != null)
+                {
+                    byte v = (byte)(value[0] & 0xF);
+                    double  v1 = (v * 256 + value[1])/4096.0 *5.0;
+                    return v1.ToString("F2");
+                }
+            }
+            catch
+            {
+                _ERROR.Visible = true;
+            }
+
+            return "Error";
+        }
+
+        private void btnReadCh0_Click(object sender, EventArgs e)
+        {
+            lbCh0.Text = ReadCh();
+        }
+
+    }
+
+
+    public class MyGUI : GuiInterface
+    {
+        public void Show(CommInterface com)
+        {
+            frmI2CADC frm = new frmI2CADC();
+            frm.Attach(com);
+            frm.ShowDialog();
+        }
+    }
+
+}
---
+++ Plugins/MS5837/I2CS/frmI2CS.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using AnyI2C;
+
+namespace MS5837_I2CS
+{
+    public partial class frmI2CS : Form
+    {
+        CommInterface CommObj = null;
+        public frmI2CS()
+        {
+            InitializeComponent();
+        }
+
+        private void frmADC081C_Load(object sender, EventArgs e)
+        {
+            numAddress.Value = CommObj.GetDefaultAddress();
+        }
+
+        public void Attach(CommInterface com)
+        {
+            CommObj = com;
+        }
+
+        /// <summary>
+        /// Send command, will process address
+        /// </summary>
+        private byte GetAddress(bool Read)
+        {
+            if (Read)
+            {
+                return (byte)(numAddress.Value * 2 + 1);
+            }
+            return (byte)(numAddress.Value * 2);
+        }
+
+        private string ReadSensor()
+        {
+            try
+            {
+                _ERROR.Visible = false;
+                byte addr = GetAddress(false);
+                // reset sensor
+                byte[] value = CommObj.Send(new byte[] { addr, 0x1E}, 0);
+                Thread.Sleep(1000);
+                double SensT1 = 34982;
+                double OffT1 = 36352;
+                double TSC = 20328;
+                double TCO = 22354;
+                double Tref = 26646;
+                double TempSens = 26146;
+                double d1 = 0;
+                double d2 = 0;
+                // convert d1
+                value = CommObj.Send(new byte[] { addr, 0x40 }, 0);
+                Thread.Sleep(100);
+                value = CommObj.Send(new byte[] { addr, 0x00 }, 3);
+                if (value != null)
+                {
+                    if (value.Length == 3)
+                    {
+                        d1 = value[0] * 256 * 256 + value[1] * 256 + value[2];
+                    }
+                }
+                //d1 = 4958179;
+                // convert d2
+                value = CommObj.Send(new byte[] { addr, 0x50 }, 0);
+                Thread.Sleep(100);
+                value = CommObj.Send(new byte[] { addr, 0x00 }, 3);
+                if (value != null)
+                {
+                    if (value.Length == 3)
+                    {
+                        d2 = value[0] * 256 * 256 + value[1] * 256 + value[2];
+                    }
+                }
+                //d2 = 6815414;
+                // this is demo, please refer the spec for accurate cal
+                double dT = d2 - Tref * 256;
+                double tt = Math.Pow(2, 23);
+                double TEMP = 2000.0 + dT * TempSens /tt;
+                double OFF = OffT1 * Math.Pow (2 ,16) + (TCO * dT) / 128.0;
+                double SENS = SensT1 * Math.Pow(2,15)+ (TSC * dT) / 256.0;
+                double P = (d1 * SENS / Math.Pow(2,21) - OFF) / Math.Pow(2 , 13) *0.1;
+                lbP.Text = P.ToString("F2") ;
+                lbT.Text = (TEMP / 100.0).ToString("F2");
+                return P.ToString("F2");
+
+            }
+            catch
+            {
+                _ERROR.Visible = true;
+            }
+
+            return "Error";
+        }
+
+        private void btnReadCh0_Click(object sender, EventArgs e)
+        {
+            ReadSensor();
+        }
+
+    }
+
+
+    public class MyGUI : GuiInterface
+    {
+        public void Show(CommInterface com)
+        {
+            frmI2CS frm = new frmI2CS();
+            frm.Attach(com);
+            frm.ShowDialog();
+        }
+    }
+
+}
