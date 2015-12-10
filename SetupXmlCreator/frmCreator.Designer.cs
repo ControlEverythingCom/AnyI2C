9f2ef6363e90ee3c1b4f8f8db8114c1a3e2549f1
xushirui <xu@controlanything.com>
2015-12-10 07:41:25 -0600

    add setup files

---
+++ SetupXmlCreator/frmCreator.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SetupXmlCreator
+{
+    partial class frmCreator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 82);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(603, 157);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.HideSelection = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 263);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(603, 179);
+            this.textBox2.TabIndex = 0;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(533, 24);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Create xml components parts for setup xml file";
+            // 
+            // textBox3
+            // 
+            this.textBox3.HideSelection = false;
+            this.textBox3.Location = new System.Drawing.Point(12, 465);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(603, 179);
+            this.textBox3.TabIndex = 0;
+            // 
+            // frmCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 647);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmCreator";
+            this.Text = "SetupXmlCreator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ SetupXmlCreator/frmCreator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Schema;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace SetupXmlCreator
+{
+    public partial class frmCreator : Form
+    {
+        public frmCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            ArrayList devices = new ArrayList();
+            var files = Directory.EnumerateFiles(".", "*I2C*.dll");
+            StringBuilder sb1 = new StringBuilder();
+            StringBuilder sb3 = new StringBuilder();
+            foreach (var d in files)
+            {
+                string filename = Path.GetFileName(d);
+                string componentName = filename.Replace(".", "");
+                string str1 = string.Format("<Component Id=\"{0}\" Guid=\"*\">", componentName );
+                string str2 = string.Format("<File Id=\"{0}\" Name=\"{1}\" DiskId=\"1\" Source=\"{1}\" KeyPath=\"yes\"></File>", componentName, filename );
+                string str3 = "</Component>";
+                sb1.AppendLine(str1);
+                sb1.AppendLine(str2);
+                sb1.AppendLine(str3);
+                string str4 = string.Format("      <ComponentRef Id=\"{0}\" />", componentName);
+                sb3.AppendLine(str4);
+                Debug.Print(filename);
+            }
+            textBox1.Text = sb1.ToString();
+
+            files = Directory.EnumerateFiles(".\\Data", "*I2C*.xml");
+            StringBuilder sb2 = new StringBuilder();
+            foreach (var d in files)
+            {
+                string filename = Path.GetFileName(d);
+                string componentName = filename.Replace(".", "");
+                string str1 = string.Format("<Component Id=\"{0}\" Guid=\"*\">", componentName);
+                string str2 = string.Format("<File Id=\"{0}\" Name=\"{1}\" DiskId=\"1\" Source=\"Data\\{1}\" KeyPath=\"yes\"></File>", componentName, filename);
+                string str3 = "</Component>";
+                sb2.AppendLine(str1);
+                sb2.AppendLine(str2);
+                sb2.AppendLine(str3);
+                string str4 = string.Format("      <ComponentRef Id=\"{0}\" />", componentName);
+                sb3.AppendLine(str4);
+                Debug.Print(filename);
+            }
+            textBox1.Text = sb1.ToString();
+            textBox2.Text = sb2.ToString();
+            textBox3.Text = sb3.ToString();
+        }
+    }
+}
