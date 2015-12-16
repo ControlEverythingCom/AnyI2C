330f7b20979b8ccdb7b8bfa78123dda3acec7486
Xu <xu@controlanything.com>
2015-12-16 11:41:48 -0600

    temp check in

---
+++ Plugins/MPU9150/I2CS/frmI2CS.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace MPU9150_I2CS
+{
+    partial class frmI2CS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.numAddress = new System.Windows.Forms.NumericUpDown();
+            this._ERROR = new System.Windows.Forms.Label();
+            this.btnReadCh0 = new System.Windows.Forms.Button();
+            this.lbAX = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbAY = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbAZ = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chkAutoUpdate = new System.Windows.Forms.CheckBox();
+            this.lbGX = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbGY = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbGZ = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lbHeading = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lbPitch = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lbRoll = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.GroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAddress)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.GroupBox2);
+            this.GroupBox1.Controls.Add(this.numAddress);
+            this.GroupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GroupBox1.Location = new System.Drawing.Point(12, 12);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(287, 68);
+            this.GroupBox1.TabIndex = 491;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Hardware Address";
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Location = new System.Drawing.Point(372, 84);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(200, 52);
+            this.GroupBox2.TabIndex = 487;
+            this.GroupBox2.TabStop = false;
+            this.GroupBox2.Text = "GroupBox2";
+            // 
+            // numAddress
+            // 
+            this.numAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numAddress.Location = new System.Drawing.Point(11, 21);
+            this.numAddress.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numAddress.Name = "numAddress";
+            this.numAddress.Size = new System.Drawing.Size(82, 35);
+            this.numAddress.TabIndex = 490;
+            this.numAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numAddress.Value = new decimal(new int[] {
+            104,
+            0,
+            0,
+            0});
+            // 
+            // _ERROR
+            // 
+            this._ERROR.BackColor = System.Drawing.Color.Red;
+            this._ERROR.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._ERROR.Location = new System.Drawing.Point(9, 102);
+            this._ERROR.Name = "_ERROR";
+            this._ERROR.Size = new System.Drawing.Size(290, 63);
+            this._ERROR.TabIndex = 514;
+            this._ERROR.Text = "ERROR: Device did Not Respond";
+            this._ERROR.Visible = false;
+            // 
+            // btnReadCh0
+            // 
+            this.btnReadCh0.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnReadCh0.Location = new System.Drawing.Point(335, 24);
+            this.btnReadCh0.Name = "btnReadCh0";
+            this.btnReadCh0.Size = new System.Drawing.Size(249, 29);
+            this.btnReadCh0.TabIndex = 517;
+            this.btnReadCh0.Text = "Read ";
+            this.btnReadCh0.UseVisualStyleBackColor = true;
+            this.btnReadCh0.Click += new System.EventHandler(this.btnReadCh0_Click);
+            // 
+            // lbAX
+            // 
+            this.lbAX.AutoSize = true;
+            this.lbAX.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lbAX.Location = new System.Drawing.Point(389, 96);
+            this.lbAX.Name = "lbAX";
+            this.lbAX.Size = new System.Drawing.Size(23, 16);
+            this.lbAX.TabIndex = 519;
+            this.lbAX.Text = "---";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(332, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 16);
+            this.label3.TabIndex = 519;
+            this.label3.Text = "AX";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(331, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 16);
+            this.label1.TabIndex = 520;
+            this.label1.Text = "AY";
+            // 
+            // lbAY
+            // 
+            this.lbAY.AutoSize = true;
+            this.lbAY.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lbAY.Location = new System.Drawing.Point(389, 128);
+            this.lbAY.Name = "lbAY";
+            this.lbAY.Size = new System.Drawing.Size(23, 16);
+            this.lbAY.TabIndex = 521;
+            this.lbAY.Text = "---";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(332, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 16);
+            this.label2.TabIndex = 522;
+            this.label2.Text = "AZ";
+            // 
+            // lbAZ
+            // 
+            this.lbAZ.AutoSize = true;
+            this.lbAZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lbAZ.Location = new System.Drawing.Point(390, 164);
+            this.lbAZ.Name = "lbAZ";
+            this.lbAZ.Size = new System.Drawing.Size(23, 16);
+            this.lbAZ.TabIndex = 523;
+            this.lbAZ.Text = "---";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chkAutoUpdate
+            // 
+            this.chkAutoUpdate.AutoSize = true;
+            this.chkAutoUpdate.Location = new System.Drawing.Point(335, 200);
+            this.chkAutoUpdate.Name = "chkAutoUpdate";
+            this.chkAutoUpdate.Size = new System.Drawing.Size(86, 17);
+            this.chkAutoUpdate.TabIndex = 524;
+            this.chkAutoUpdate.Text = "Auto Update";
+            this.chkAutoUpdate.UseVisualStyleBackColor = true;
+            this.chkAutoUpdate.CheckedChanged += new System.EventHandler(this.chkAutoUpdate_CheckedChanged);
+            // 
+            // lbGX
+            // 
+            this.lbGX.AutoSize = true;
+            this.lbGX.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lbGX.Location = new System.Drawing.Point(536, 96);
+            this.lbGX.Name = "lbGX";
+            this.lbGX.Size = new System.Drawing.Size(23, 16);
+            this.lbGX.TabIndex = 519;
+            this.lbGX.Text = "---";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(479, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 16);
+            this.label5.TabIndex = 519;
+            this.label5.Text = "GX";
+            // 
+            // lbGY
+            // 
+            this.lbGY.AutoSize = true;
+            this.lbGY.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lbGY.Location = new System.Drawing.Point(536, 128);
+            this.lbGY.Name = "lbGY";
+            this.lbGY.Size = new System.Drawing.Size(23, 16);
+            this.lbGY.TabIndex = 521;
+            this.lbGY.Text = "---";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(478, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 16);
+            this.label7.TabIndex = 520;
+            this.label7.Text = "GY";
+            // 
+            // lbGZ
+            // 
+            this.lbGZ.AutoSize = true;
+            this.lbGZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lbGZ.Location = new System.Drawing.Point(537, 164);
+            this.lbGZ.Name = "lbGZ";
+            this.lbGZ.Size = new System.Drawing.Size(23, 16);
+            this.lbGZ.TabIndex = 523;
+            this.lbGZ.Text = "---";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(479, 164);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(28, 16);
+            this.label9.TabIndex = 522;
+            this.label9.Text = "GZ";
+            // 
+            // lbHeading
+            // 
+            this.lbHeading.AutoSize = true;
+            this.lbHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lbHeading.Location = new System.Drawing.Point(710, 100);
+            this.lbHeading.Name = "lbHeading";
+            this.lbHeading.Size = new System.Drawing.Size(23, 16);
+            this.lbHeading.TabIndex = 519;
+            this.lbHeading.Text = "---";
+            this.lbHeading.Visible = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label11.Location = new System.Drawing.Point(637, 100);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 16);
+            this.label11.TabIndex = 519;
+            this.label11.Text = "Heading";
+            this.label11.Visible = false;
+            // 
+            // lbPitch
+            // 
+            this.lbPitch.AutoSize = true;
+            this.lbPitch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lbPitch.Location = new System.Drawing.Point(710, 128);
+            this.lbPitch.Name = "lbPitch";
+            this.lbPitch.Size = new System.Drawing.Size(23, 16);
+            this.lbPitch.TabIndex = 521;
+            this.lbPitch.Text = "---";
+            this.lbPitch.Visible = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label13.Location = new System.Drawing.Point(636, 132);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(42, 16);
+            this.label13.TabIndex = 520;
+            this.label13.Text = "Picth";
+            this.label13.Visible = false;
+            // 
+            // lbRoll
+            // 
+            this.lbRoll.AutoSize = true;
+            this.lbRoll.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lbRoll.Location = new System.Drawing.Point(710, 168);
+            this.lbRoll.Name = "lbRoll";
+            this.lbRoll.Size = new System.Drawing.Size(23, 16);
+            this.lbRoll.TabIndex = 523;
+            this.lbRoll.Text = "---";
+            this.lbRoll.Visible = false;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label15.Location = new System.Drawing.Point(637, 168);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(36, 16);
+            this.label15.TabIndex = 522;
+            this.label15.Text = "Roll";
+            this.label15.Visible = false;
+            // 
+            // frmI2CS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 239);
+            this.Controls.Add(this.chkAutoUpdate);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbRoll);
+            this.Controls.Add(this.lbGZ);
+            this.Controls.Add(this.lbAZ);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbPitch);
+            this.Controls.Add(this.lbGY);
+            this.Controls.Add(this.lbAY);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.lbHeading);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbGX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbAX);
+            this.Controls.Add(this._ERROR);
+            this.Controls.Add(this.btnReadCh0);
+            this.Name = "frmI2CS";
+            this.Text = "MPU9150 Control Panel";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmI2CS_FormClosing);
+            this.Load += new System.EventHandler(this.L3G4200_Load);
+            this.GroupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numAddress)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.GroupBox GroupBox2;
+        internal System.Windows.Forms.NumericUpDown numAddress;
+        internal System.Windows.Forms.Label _ERROR;
+        internal System.Windows.Forms.Button btnReadCh0;
+        internal System.Windows.Forms.Label lbAX;
+        internal System.Windows.Forms.Label label3;
+        internal System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label lbAY;
+        internal System.Windows.Forms.Label label2;
+        internal System.Windows.Forms.Label lbAZ;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox chkAutoUpdate;
+        internal System.Windows.Forms.Label lbGX;
+        internal System.Windows.Forms.Label label5;
+        internal System.Windows.Forms.Label lbGY;
+        internal System.Windows.Forms.Label label7;
+        internal System.Windows.Forms.Label lbGZ;
+        internal System.Windows.Forms.Label label9;
+        internal System.Windows.Forms.Label lbHeading;
+        internal System.Windows.Forms.Label label11;
+        internal System.Windows.Forms.Label lbPitch;
+        internal System.Windows.Forms.Label label13;
+        internal System.Windows.Forms.Label lbRoll;
+        internal System.Windows.Forms.Label label15;
+    }
+}
