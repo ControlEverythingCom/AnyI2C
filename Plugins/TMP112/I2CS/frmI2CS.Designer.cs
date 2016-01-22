ff2dadddf2fa4eca4fefef2e7df02eeb5dc9891f
gurvindrasingh <gurvindrasingh@gmail.com>
2016-01-22 11:06:14 +0530

    add tmp112

---
+++ Plugins/TMP112/I2CS/frmI2CS.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace TMP112_I2CS
+{
+    partial class frmI2CS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numAddress = new System.Windows.Forms.NumericUpDown();
+            this.btnReadCh0 = new System.Windows.Forms.Button();
+            this._ERROR = new System.Windows.Forms.Label();
+            this.lbCh0 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAddress)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(224, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numAddress);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 68);
+            this.groupBox1.TabIndex = 491;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hardware Address";
+            // 
+            // numAddress
+            // 
+            this.numAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
+            this.numAddress.Location = new System.Drawing.Point(11, 21);
+            this.numAddress.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numAddress.Name = "numAddress";
+            this.numAddress.Size = new System.Drawing.Size(82, 35);
+            this.numAddress.TabIndex = 490;
+            this.numAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numAddress.Value = new decimal(new int[] {
+            72,
+            0,
+            0,
+            0});
+            // 
+            // btnReadCh0
+            // 
+            this.btnReadCh0.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnReadCh0.Location = new System.Drawing.Point(335, 24);
+            this.btnReadCh0.Name = "btnReadCh0";
+            this.btnReadCh0.Size = new System.Drawing.Size(249, 29);
+            this.btnReadCh0.TabIndex = 517;
+            this.btnReadCh0.Text = "Read ";
+            this.btnReadCh0.UseVisualStyleBackColor = true;
+            this.btnReadCh0.Click += new System.EventHandler(this.btnReadCh0_Click);
+            // 
+            // _ERROR
+            // 
+            this._ERROR.BackColor = System.Drawing.Color.Red;
+            this._ERROR.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this._ERROR.Location = new System.Drawing.Point(9, 102);
+            this._ERROR.Name = "_ERROR";
+            this._ERROR.Size = new System.Drawing.Size(290, 63);
+            this._ERROR.TabIndex = 514;
+            this._ERROR.Text = "ERROR: Device did Not Respond";
+            this._ERROR.Visible = false;
+            // 
+            // lbCh0
+            // 
+            this.lbCh0.AutoSize = true;
+            this.lbCh0.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lbCh0.Location = new System.Drawing.Point(414, 132);
+            this.lbCh0.Name = "lbCh0";
+            this.lbCh0.Size = new System.Drawing.Size(23, 16);
+            this.lbCh0.TabIndex = 521;
+            this.lbCh0.Text = "---";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(476, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 16);
+            this.label2.TabIndex = 520;
+            this.label2.Text = "C";
+            // 
+            // frmI2CS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 196);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbCh0);
+            this.Controls.Add(this._ERROR);
+            this.Controls.Add(this.btnReadCh0);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmI2CS";
+            this.Text = "TMP112 Control Panel";
+            this.Load += new System.EventHandler(this.frmTMP112_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numAddress)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.GroupBox groupBox1;
+        internal System.Windows.Forms.NumericUpDown numAddress;
+        internal System.Windows.Forms.Button btnReadCh0;
+        internal System.Windows.Forms.Label _ERROR;
+        internal System.Windows.Forms.Label lbCh0;
+        internal System.Windows.Forms.Label label2;
+    }
+}
---
+++ Plugins/TMP112/I2CS/frmI2CS.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using AnyI2C;
+
+namespace TMP112_I2CS
+{
+    public partial class frmI2CS : Form
+    {
+        CommInterface CommObj = null;
+        public frmI2CS()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTMP112_Load(object sender, EventArgs e)
+        {
+            numAddress.Value = CommObj.GetDefaultAddress();
+        }
+
+        public void Attach(CommInterface com)
+        {
+            CommObj = com;
+        }
+
+        /// <summary>
+        /// Send command, will process address
+        /// </summary>
+        private byte GetAddress(bool Read)
+        {
+            if (Read)
+            {
+                return (byte)(numAddress.Value * 2 + 1);
+            }
+            return (byte)(numAddress.Value * 2);
+        }
+
+        private string ReadSensor()
+        {
+            try
+            {
+                _ERROR.Visible = false;
+                byte addr = GetAddress(false);
+                byte[] value = CommObj.Send(new byte[] { addr, 0 }, 2);
+                if (value != null)
+                {
+                    double t = ((value[0] << 8) | value[1]) / 256.0;
+                    lbCh0.Text = t.ToString("F2");
+                    return value[0].ToString();
+                }
+            }
+            catch
+            {
+                _ERROR.Visible = true;
+            }
+
+            return "Error";
+        }
+
+        private void btnReadCh0_Click(object sender, EventArgs e)
+        {
+            ReadSensor();
+        }
+    }
+
+    public class MyGUI : GuiInterface
+    {
+        public void Show(CommInterface com)
+        {
+            frmI2CS frm = new frmI2CS();
+            frm.Attach(com);
+            frm.ShowDialog();
+        }
+    }
+}
