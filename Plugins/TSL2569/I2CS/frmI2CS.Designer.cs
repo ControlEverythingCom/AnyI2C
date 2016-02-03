3c9c062b791800254225a135e329fb340e7a1b69
gurvindrasingh <gurvindrasingh@gmail.com>
2016-02-03 18:23:03 +0530

    Added TSL2569 Plugin

---
+++ Plugins/TSL2569/I2CS/frmI2CS.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace TSL2569_I2CS
+{
+    partial class frmI2CS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.chkAutoUpdate = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbCh0 = new System.Windows.Forms.Label();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.numAddress = new System.Windows.Forms.NumericUpDown();
+            this._ERROR = new System.Windows.Forms.Label();
+            this.btnReadCh0 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.GroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAddress)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chkAutoUpdate
+            // 
+            this.chkAutoUpdate.AutoSize = true;
+            this.chkAutoUpdate.Location = new System.Drawing.Point(341, 170);
+            this.chkAutoUpdate.Name = "chkAutoUpdate";
+            this.chkAutoUpdate.Size = new System.Drawing.Size(86, 17);
+            this.chkAutoUpdate.TabIndex = 532;
+            this.chkAutoUpdate.Text = "Auto Update";
+            this.chkAutoUpdate.UseVisualStyleBackColor = true;
+            this.chkAutoUpdate.CheckedChanged += new System.EventHandler(this.chkAutoUpdate_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(476, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 16);
+            this.label1.TabIndex = 530;
+            this.label1.Text = "Lux";
+            // 
+            // lbCh0
+            // 
+            this.lbCh0.AutoSize = true;
+            this.lbCh0.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lbCh0.Location = new System.Drawing.Point(404, 132);
+            this.lbCh0.Name = "lbCh0";
+            this.lbCh0.Size = new System.Drawing.Size(23, 16);
+            this.lbCh0.TabIndex = 531;
+            this.lbCh0.Text = "---";
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.GroupBox2);
+            this.GroupBox1.Controls.Add(this.numAddress);
+            this.GroupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GroupBox1.Location = new System.Drawing.Point(12, 12);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(287, 68);
+            this.GroupBox1.TabIndex = 527;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Hardware Address";
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Location = new System.Drawing.Point(372, 84);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(200, 52);
+            this.GroupBox2.TabIndex = 487;
+            this.GroupBox2.TabStop = false;
+            this.GroupBox2.Text = "GroupBox2";
+            // 
+            // numAddress
+            // 
+            this.numAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numAddress.Location = new System.Drawing.Point(11, 21);
+            this.numAddress.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numAddress.Name = "numAddress";
+            this.numAddress.Size = new System.Drawing.Size(82, 35);
+            this.numAddress.TabIndex = 490;
+            this.numAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numAddress.Value = new decimal(new int[] {
+            57,
+            0,
+            0,
+            0});
+            // 
+            // _ERROR
+            // 
+            this._ERROR.BackColor = System.Drawing.Color.Red;
+            this._ERROR.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._ERROR.Location = new System.Drawing.Point(9, 102);
+            this._ERROR.Name = "_ERROR";
+            this._ERROR.Size = new System.Drawing.Size(290, 63);
+            this._ERROR.TabIndex = 528;
+            this._ERROR.Text = "ERROR: Device did Not Respond";
+            this._ERROR.Visible = false;
+            // 
+            // btnReadCh0
+            // 
+            this.btnReadCh0.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnReadCh0.Location = new System.Drawing.Point(335, 24);
+            this.btnReadCh0.Name = "btnReadCh0";
+            this.btnReadCh0.Size = new System.Drawing.Size(249, 29);
+            this.btnReadCh0.TabIndex = 529;
+            this.btnReadCh0.Text = "Read ";
+            this.btnReadCh0.UseVisualStyleBackColor = true;
+            this.btnReadCh0.Click += new System.EventHandler(this.btnReadCh0_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmI2CS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 239);
+            this.Controls.Add(this.chkAutoUpdate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbCh0);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this._ERROR);
+            this.Controls.Add(this.btnReadCh0);
+            this.Name = "frmI2CS";
+            this.Text = "TSL2569 Control Panel";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmI2CS_FormClosing);
+            this.Load += new System.EventHandler(this.frmTSL2569_Load);
+            this.GroupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numAddress)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkAutoUpdate;
+        internal System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label lbCh0;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.GroupBox GroupBox2;
+        internal System.Windows.Forms.NumericUpDown numAddress;
+        internal System.Windows.Forms.Label _ERROR;
+        internal System.Windows.Forms.Button btnReadCh0;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
