7bbde7e2750fa3165b51fc38441b434bfa212211
gurvindrasingh <gurvindrasingh@gmail.com>
2016-04-06 17:28:04 +0530

    Added AMS5812_0050DB

---
+++ AnyI2C_windows/Plugins/AMS5812_0050DB/I2CS/frmI2CS.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using AnyI2C;
+
+namespace AMS5812_0050DB_I2CS
+{
+    public partial class frmI2CS : Form
+    {
+        CommInterface CommObj = null;
+        public frmI2CS()
+        {
+            InitializeComponent();
+        }
+
+        private void frmI2CS_Load(object sender, EventArgs e)
+        {
+            CommObj.LogText("GUI Opened");
+            numAddress.Value = CommObj.GetDefaultAddress();
+            numPort.Value = CommObj.GetPort();
+        }
+
+        public void Attach(CommInterface com)
+        {
+            CommObj = com;
+        }
+
+        /// <summary>
+        /// Send command, will process address
+        /// </summary>
+        private byte GetAddress(bool Read)
+        {
+            if (Read)
+            {
+                return (byte)(numAddress.Value * 2 + 1);
+            }
+            return (byte)(numAddress.Value * 2);
+        }
+
+        private string ReadSensor()
+        {
+            try
+            {
+                _ERROR.Visible = false;
+                byte addr = GetAddress(false);
+                //Read Data
+                byte[] value = CommObj.Send(new byte[] { addr , 0x00 }, 4);
+                Thread.Sleep(500);
+                if (value != null)
+                {
+                    if (value.Length == 4)
+                    {
+                        double pressure = ((value[0] & 0xFF) * 256 + (value[1] & 0xFF));
+                        double temp = ((value[2] & 0xFF) * 256 + (value[3] & 0xFF));
+                        pressure = ((pressure - 3277.0) / ((26214.0) / 10.0)) - 5.0;
+                        double cTemp = ((temp - 3277.0) / ((26214.0) / 110.0)) - 25.0;
+                        lbC.Text = cTemp.ToString("F2");
+                        lbP.Text = pressure.ToString("F2");
+                    }
+                }
+                return "OK";
+            }
+            catch
+            {
+                _ERROR.Visible = true;
+            }
+
+            return "Error";
+        }
+
+        private void frmI2CS_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Enabled = false;
+            CommObj.LogText("GUI Closed");
+        }
+
+        private void numPort_ValueChanged(object sender, EventArgs e)
+        {
+            CommObj.SetPort((byte)numPort.Value);
+        }
+
+        private void btnReadCh0_Click(object sender, EventArgs e)
+        {
+            ReadSensor();
+        }
+
+        private void chkAutoUpdate_CheckedChanged(object sender, EventArgs e)
+        {
+            btnReadCh0.Enabled = !chkAutoUpdate.Checked;
+            if (chkAutoUpdate.Checked)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ReadSensor();
+        }
+    }
+
+    public class MyGUI : GuiInterface
+    {
+        public void Show(CommInterface com)
+        {
+            frmI2CS frm = new frmI2CS();
+            frm.Attach(com);
+            frm.ShowDialog();
+        }
+    }
+}
