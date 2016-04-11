04fb41abb6ef90413cba40119a0594e9e27e429e
gurvindrasingh <gurvindrasingh@gmail.com>
2016-04-11 17:23:25 +0530

    Added AT24HC02C and ADC121C021 SM72480 Plugin

---
+++ AnyI2C_windows/Plugins/ADC121C021_SM72480/I2CADC/frmI2CADC.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using AnyI2C;
+
+namespace ADC121C021_SM72480_I2CADC
+{
+    public partial class frmI2CADC : Form
+    {
+        CommInterface CommObj = null;
+        public frmI2CADC()
+        {
+            InitializeComponent();
+        }
+
+        private void frmI2CADC_Load(object sender, EventArgs e)
+        {
+            CommObj.LogText("GUI Opened");
+            numAddress.Value = CommObj.GetDefaultAddress();
+            numPort.Value = CommObj.GetPort();
+        }
+
+        public void Attach(CommInterface com)
+        {
+            CommObj = com;
+        }
+
+        /// <summary>
+        /// Send command, will process address
+        /// </summary>
+        private byte GetAddress(bool Read)
+        {
+            if (Read)
+            {
+                return (byte)(numAddress.Value * 2 + 1);
+            }
+            return (byte)(numAddress.Value * 2);
+        }
+
+        private string ReadCh()
+        {
+            try
+            {
+                _ERROR.Visible = false;
+                byte addr = GetAddress(false);
+                byte[] value = CommObj.Send(new byte[] { addr, 0x02, 0x20 }, 0);
+                Thread.Sleep(500);
+                value = CommObj.Send(new byte[] { addr, 0x00 }, 2);
+                if (value != null)
+                {
+                    lbCh1.Text = ((value[0] & 0x0F) * 256 + value[1]).ToString("F2");
+                }
+            }
+            catch
+            {
+                _ERROR.Visible = true;
+            }
+
+            return "Error";
+        }
+
+        private void btnReadCh0_Click(object sender, EventArgs e)
+        {
+            ReadCh();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ReadCh();
+        }
+
+        private void chkAutoUpdate_CheckedChanged(object sender, EventArgs e)
+        {
+            btnReadCh0.Enabled = !chkAutoUpdate.Checked;
+            if (chkAutoUpdate.Checked)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void frmI2CADC_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Enabled = false;
+            CommObj.LogText("GUI Closed");
+        }
+
+        private void numPort_ValueChanged(object sender, EventArgs e)
+        {
+            CommObj.SetPort((byte)numPort.Value);
+        } 
+    }
+
+    public class MyGUI : GuiInterface
+    {
+        public void Show(CommInterface com)
+        {
+            frmI2CADC frm = new frmI2CADC();
+            frm.Attach(com);
+            frm.ShowDialog();
+        }
+    }
+}
